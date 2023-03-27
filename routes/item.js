c5375c3bfa148bc007b5ceb2f0201d3278c7cd24
mayssabj <maissabenjoud@gmail.com>
2023-03-27 23:30:55 +0100

    maissa updates

--- routes/item.js
+++ routes/item.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const router = express.Router();
-module.exports = (app) => {
-    
-    const items = require('../controllers/item.controller');
+const Item = require('../models/item');
 
-    app.post('/', items.create);
+// Read data
+router.get('/items', (req, res) => {
+  Item.find({})
+    .then(items => {
+      res.json(items);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: 'Error reading items' });
+    });
+});
 
-    
-    app.get('/items', itemss.findAll);
 
-   
-   
-}
+router.post('/', function (req, res) {
+    let newItem = new Item({
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+      Address:req.body.Address,
+      City:req.body.City,
+      State:req.body.State,
+      title: req.body.title,
+      description: req.body.description,
+      phone:req.body.phone,
+      type:req.body.type,
+      image:req.body.image
+    });
+    newItem.save();
+    res.redirect('/listing-layout-2.html')
+  });
+  module.exports = router;
