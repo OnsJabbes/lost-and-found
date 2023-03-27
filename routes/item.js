2fc5de86a57e967297c277143932fe143a2ae294
OnsJabbes <onsjabes@gmail.com>
2023-03-28 00:11:29 +0200

    ons update

--- routes/item.js
+++ routes/item.js
@@ -1,34 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const Item = require('../models/item');
+module.exports = (app) => {
+    
+    const items = require('../controllers/item.controller');
 
-// Read data
-router.get('/items', (req, res) => {
-  Item.find({})
-    .then(items => {
-      res.json(items);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({ error: 'Error reading items' });
-    });
-});
+    app.post('/', items.create);
 
+    
+    app.get('/items', itemss.findAll);
 
-router.post('/', function (req, res) {
-    let newItem = new Item({
-      latitude: req.body.latitude,
-      longitude: req.body.longitude,
-      Address:req.body.Address,
-      City:req.body.City,
-      State:req.body.State,
-      title: req.body.title,
-      description: req.body.description,
-      phone:req.body.phone,
-      type:req.body.type,
-      image:req.body.image
-    });
-    newItem.save();
-    res.redirect('/listing-layout-2.html')
-  });
-  module.exports = router;
+   
+   
+}
