18f460e379b701905d79259c58677028a6c5ccb1
mayssabj <maissabenjoud@gmail.com>
2023-06-06 14:24:38 +0100

    mayssa updates

--- routes/admin.js
+++ routes/admin.js
@@ -7,38 +7,43 @@ const mongoose = require('mongoose');
 
 router.get('/itemtable', (req, res) => {
 
-    Item.find({ })
-      .then(items => {
-        if (items.length === 0) {
-          return res.status(404).redirect("../error-page.html");
-        }
-        res.render('../table-item.html',{ items });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({ error: 'Error reading items' });
-      });
-    
-      
-      
-  });
+  Item.find({})
+    .then(items => {
+      if (items.length === 0) {
+        return res.status(404).redirect("../error-page.html");
+      }
+      res.render('../table-item.html', { items });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: 'Error reading items' });
+    });
 
 
-  router.get('/usertable', (req, res) => {
-    User.find({ })
-        .then(users => {
-        if (users.length === 0) {
-          return res.status(404).redirect("../error-page.html");
-        }
-        res.render('../table-user.html',{ users });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({ error: 'Error reading items' });
-      });
+
+});
+router.get('/logout', (req, res) => {
+  res.clearCookie('session');
+  res.redirect('./login.html');
+});
 
 
-      
-      
-  });
-  module.exports = router;
+
+router.get('/usertable', (req, res) => {
+  User.find({})
+    .then(users => {
+      if (users.length === 0) {
+        return res.status(404).redirect("../error-page.html");
+      }
+      res.render('../table-user.html', { users });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: 'Error reading items' });
+    });
+
+
+
+
+});
+module.exports = router;
